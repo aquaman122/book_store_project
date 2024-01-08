d8e9a91f523c65ae23a230543a46f6f84297a0b4
aquaman122 <swj981128@gmail.com>
2024-01-08 14:19:02 +0900

    commit: remove orders

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -13,27 +13,5 @@ require("dotenv").config();
 
 router.use(express.json());
 
-const isValidate = (req, res, next) => {
-  const err = validationResult(req);
-
-  if (err.isEmpty()) {
-    return next();
-  }
-  res.status(400).json(err.array());
-};
-
-const isError = (res) => {
-  return res.status(404).json({
-    message: "오류임.."
-  });
-};
-
-// 장바구니 담기 POST 201
-
-// 장바구니 조회 200
-
-// 장바구니 제거 200
-
-// 장바구니 일괄 삭제 200
 
 module.exports = router;
