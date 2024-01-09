b98e10926bf6160e9bc7548ed3060fa85a7b5c08
aquaman122 <swj981128@gmail.com>
2024-01-09 19:48:14 +0900

    commit: remove some...

--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -2,15 +2,8 @@ const express = require("express");
 const router = express.Router();
 // validator
 const {body, validationResult} = require('express-validator');
-// connection mariadb module
-const conn = require('../mariadb');
-
-// JWT module
-const jwt = require('jsonwebtoken');
 const { addToCart, getCartItems, removeCartItems } = require('../controller/cartController');
 
-// dotenv module and declear
-require("dotenv").config();
 
 router.use(express.json());
 
