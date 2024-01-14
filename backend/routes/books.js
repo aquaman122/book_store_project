0f7b4a55c55d14c9f2e442ddbbfdd21d15c142f8
aquaman122 <swj981128@gmail.com>
2024-01-14 15:16:01 +0900

    refactoring: router.use 반복제거

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 // connection mariadb module
-router.use(express.json());
 
 const {
   allBooks,
--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -4,10 +4,6 @@ const router = express.Router();
 const {body, validationResult} = require('express-validator');
 const { addToCart, getCartItems, removeCartItems } = require('../controller/cartController');
 
-
-router.use(express.json());
-
-
 // 장바구니 담기 POST 201
 router.post('/', addToCart)
 // 장바구니 조회 200 // 장바구니 예상 상품 목록 조희
