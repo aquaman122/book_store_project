4b14dd26d4cc3ef18f017442ac0a157c29d31e5c
aquaman122 <swj981128@gmail.com>
2024-02-22 20:36:00 +0900

    commit: add contextAPI

--- frontend/src/style/theme.ts
+++ frontend/src/style/theme.ts
@@ -1,27 +1,111 @@
 export type ThemeName = "light" | "dark";
-type ColorKey = "primary" | "background" | "secondary" | "third";
+export type ColorKey = "primary" | "background" | "secondary" | "third" | "border" | "text";
+export type HeadingSize = "large" | "medium" | "small";
+export type ButtonSize = "large" | "medium" | "small";
+export type ButtonScheme = "primary" | "normal";
+export type LayoutWidth = "large" | "medium" | "small";
 
-interface Theme {
-  name : ThemeName;
+export interface Theme {
+  name: ThemeName,
   color: Record<ColorKey, string>;
+  heading: Record<HeadingSize, {fontSize: string}>;
+  button: {
+    [key in ButtonSize]: {
+      fontSize: string;
+      padding: string;
+    }
+  };
+  buttonScheme: {
+    [key in ButtonScheme]: {
+      color: string;
+      backgroundColor: string;
+    }
+  };
+  borderRadius: {
+    default: string;
+  }
+  layout: {
+    width: {
+      [key in LayoutWidth]: string;
+    }
+  }
 }
 
-export const light = {
+export const light: Theme = {
   name: 'light',
   color : {
-    primary: "brown",
-    background: "lightgray",
-    secondary: "blue",
+    primary: "#ff5800",
+    background: "lightgrey",
+    secondary: "#5F5F5F",
     third : "green",
+    border: "gray",
+    text: "black"
+  },
+  heading: {
+    large: {
+      fontSize: "2rem"
+    },
+    medium: {
+      fontSize: "1.5rem"
+    },
+    small: {
+      fontSize: "1rem"
+    },
+  },
+  button: {
+    large: {
+      fontSize: "1.5rem",
+      padding: "1rem 2rem",
+    },
+    medium: {
+      fontSize: "1rem",
+      padding: "0.5rem 1rem",
+    },
+    small: {
+      fontSize: "0.75rem",
+      padding: "0.25rem 0.5rem",
+    },
+  },
+  buttonScheme: {
+    primary: {
+      color: "white",
+      backgroundColor: "midnightblue"
+    },
+    normal: {
+      color: "black",
+      backgroundColor: "lightgrey"
+    }
+  },
+  borderRadius: {
+    default: "4px"
+  },
+  layout: {
+    width: {
+      large: "1020px",
+      medium: "760px",
+      small: "320px",
+    },
   },
 };
 
-export const dark = {
+export const dark: Theme = {
+  ...light,
   name: "dark",
   color : {
     primary: "coral",
     background: "midnightblue",
     secondary: "darkblue",
     third : "darkgreen",
+    border: "gray",
+    text: "black",
+  }
+}
+
+export const getTheme = (themeName: ThemeName): Theme => {
+  switch(themeName) {
+    case "light":
+      return light;
+    case "dark":
+      return dark;
   }
 }
