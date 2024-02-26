62f7d005ebf7a0d7ad98e2311eb0158683d9cafa
aquaman122 <swj981128@gmail.com>
2024-02-26 16:55:36 +0900

    add: books page

---
+++ frontend/src/components/books/Pagination.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+function Pagination() {
+  return (
+    <>
+      <PaginationStyle>
+       
+      </PaginationStyle>
+    </>
+  )
+}
+
+const PaginationStyle = styled.div`
+
+`;
+
+export default Pagination;
